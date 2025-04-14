1ebf045987f3eb148f3c60d1095bb71b3a38d07b
revyark <anantiy42@gmail.com>
2025-04-14 17:31:39 +0530

    host set to 0000

--- app.py
+++ app.py
@@ -165,4 +165,4 @@ def predict():
     return render_template('result.html', cluster=cluster, meaning=meaning)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
